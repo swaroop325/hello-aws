ede1d91f9cb3eeeb5351a2b0707d312f3bc63763
swaroop.anand <swaroop.anand@activate.sg>
2025-01-15 22:41:38 +0800

    [Swaroop] Added S3 Support

--- src/config/redisConfig.js
+++ src/config/redisConfig.js
@@ -1,23 +1,19 @@
 const { REDIS_PORT, REDIS_HOST } = require("../constants");
 const redis = require("redis");
 
-// Redis client configuration
-const redisClient = redis.createClient({
-  socket: {
-    host: REDIS_HOST || "localhost",
-    port: REDIS_PORT,
-  },
-  password: process.env.REDIS_PASSWORD || undefined,
-});
-
-// Connect to Redis
-redisClient.connect().catch((err) => {
-  console.error("Redis connection error:", err);
-});
-
 // Check Redis connection health
 const checkRedisConnection = async () => {
   try {
+    const redisClient = redis.createClient({
+      socket: {
+        host: REDIS_HOST || "localhost",
+        port: REDIS_PORT,
+      },
+      password: process.env.REDIS_PASSWORD || undefined,
+    });
+    redisClient.connect().catch((err) => {
+      console.error("Redis connection error:", err);
+    });
     await redisClient.ping(); // Ping Redis server
     console.log("Redis connection is healthy");
     return true;
